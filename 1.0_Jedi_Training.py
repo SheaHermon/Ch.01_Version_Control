11d029aac0e418d139e8824d99647cbe1ae9d91d
Shea <hermon.oshea@carlislecsd.org>
2023-06-02 22:04:48 -0500

    Finished again

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -1,21 +1,21 @@
 '''
-Sign your name:________________
+Sign your name:Shea Hermon
 
 
 
-Define Forking: 
+Define Forking: When someone makes a copy of a shared repository.
 
-Define Cloning: 
+Define Cloning: put on the machine so code can be worked on
 
-Define Branching:
+Define Branching: copy of original when created but can be edited
 
-Define Committing: 
+Define Committing: it will let you see what you changed and let you force save
 
-Define Merging: 
+Define Merging: You can combine branches so that they both have the same stuff
 
-Define Pushing:
+Define Pushing: It puts everything you did into the repository
 
-Define Pull Request:
+Define Pull Request: It allows you to ask something from the creator of repository
 
 
 '''
